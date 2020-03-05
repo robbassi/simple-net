1d2c344967b1a85072e2b2524b227fa688c5bf7d
Rob Bassi <rob.bassi.89@gmail.com>
2020-03-04 22:00:44 -0500

    Cleanup

--- src/Train.hs
+++ src/Train.hs
@@ -11,13 +11,13 @@ randRs n = replicateM n . liftRand . randomR
 train :: Net -> [([Double],[Double])] -> Int -> Double -> Rand StdGen Net
 train net _ 0 _ = return net
 train net samples epochs learnRate = do
-  sampleR <- randRs (length . fst $ samples !! 0) (0, (pred $ length samples))
+  sampleR <- randRs sampleSize (0, (pred $ length samples))
   let samples' = (samples !!) <$> sampleR
       inputs = fromLists (fst <$> samples')
       targets = fromLists (snd <$> samples')
       (net', predicted) = predict net inputs
-      --epochLoss' = epochLoss + (netLoss net) predicted targets
       grad = (netGrad net) predicted targets
       (net'', _) = backPropagate net' grad
       net''' = (netOpt net) learnRate net''
   train net''' samples (pred epochs) learnRate
+  where sampleSize = length . fst $ samples !! 0
